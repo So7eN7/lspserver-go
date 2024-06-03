b2851c46d1ade5b3809e7247ae481c8a02b01684
so7en <soorenaaryan7@protonmail.com>
2024-06-03 15:02:46 +0330

    logger function

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"bufio"
-	"fmt"
+	"log"
 	"lspserver_go/rpc"
 	"os"
 )
 
 func main() {
-  fmt.Println("test")
+  logger := getLogger("/home/so7en/Desktop/lspserver-go/log.txt")
+  logger.Println("logger started...")
 
   scanner := bufio.NewScanner(os.Stdin)
   scanner.Split(rpc.Split)
@@ -17,6 +18,14 @@ func main() {
     msg := scanner.Text()
     HandleMessage(msg)  
   }
+}
+
+func getLogger(filename string) *log.Logger {
+  logfile, err := os.OpenFile(filename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666) // 0666 -> creatiing file
+  if err != nil {
+    panic("wrong file")
+  }
+  return log.New(logfile, "[lspserver_go]", log.Ldate|log.Ltime|log.Lshortfile)
 }
 
 func HandleMessage (msg any) {
